f90b1aef39ee3401028734dd4184ef81d4b4710d
xiaowu <hadoopsf@163.com>
2020-12-14 23:55:37 +0800

    创建项目

---
+++ src/main/java/com/tangmen/wudijinshen.java
@@ -0,0 +1,10 @@
+package com.tangmen;
+
+public class wudijinshen {
+    public static void main(String[] args) {
+        System.out.println("三哥");
+        System.out.println("三哥");
+        System.out.println("三哥");
+        System.out.println("三哥");
+    }
+}
