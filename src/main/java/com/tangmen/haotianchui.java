e54ed632da435d7787a1b028058f5ee17f527159
xiaowu <hadoopsf@163.com>
2020-12-15 00:32:41 +0800

    武魂分支

---
+++ src/main/java/com/tangmen/haotianchui.java
@@ -0,0 +1,11 @@
+package com.tangmen;
+
+public class haotianchui {
+    public static void main(String[] args) {
+        System.out.println("天下diyi");
+        System.out.println("天下diyi");
+        System.out.println("天下diyi");
+
+
+    }
+}
